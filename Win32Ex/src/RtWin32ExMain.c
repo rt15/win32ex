f5d999a1f7d28f7e54b0e95a69508aa1cfecd4b5
rt15 <boissard.bruno@gmail.com>
2018-08-09 11:32:22 +0200

    Add some documentation about argc/argv

--- Win32Ex/src/RtWin32ExMain.c
+++ Win32Ex/src/RtWin32ExMain.c
@@ -10,6 +10,23 @@
  * <p>
  * It must not include RtWin32ExMain.h because it would define symbols like main/WinMainCRTStartup/mainCRTStartup.
  * </p>
+ *
+ * <p>
+ * Under Linux, when we launch a command in a shell, the shell builds argc/argv and calls <tt>execve</tt>.<br>
+ * So it is the responsibility of the process creator to build argv/argc.<br>
+ * The entry point of an ELF file is defined by the <tt>e_entry</tt> field of the ELF header (It usually points on a <tt>_start</tt> symbol).<br>
+ * When the entry point is called, argc is at the top of the stack, then there is argv[0], argv[1]... then NULL, then envp[0], envp[1]... and NULL.<br>
+ * As a result the entry point does not follow any classical calling convention such as <tt>cdecl</tt>.<br>
+ * It is probably impossible to implement a _start function in C that reads argv.
+ * </p>
+ *
+ * <p>
+ * Under Windows, processes are created through a command line.<br>
+ * So it is the responsibility of the created process to build argv/argc, if it needs them.<br>
+ * The entry point is specified by the AddressOfEntryPoint field of the COFF header.<br>
+ * The symbol of the entry point is often WinMainCRTStartup for a GUI application and mainCRTStartup for console application.<br>
+ * The entry point doesn't take any argument. Argv/argc can be built using GetCommandLineW then CommandLineToArgvW.<br>
+ * </p>
  */
 
 #ifdef RT_DEFINE_WINDOWS
